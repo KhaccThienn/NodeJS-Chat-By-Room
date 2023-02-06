7af27ed7af864152d1d6055a2d843598d3ca6701
Lê Khắc Thiện <muzankibut112@gmail.com>
2023-02-06 11:52:34 +0700

    Final join in chat, chieu lam chat

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/login.html'));
+});
+
+var listUsers = [];
+
+io.on("connection", (socket) => {
+    socket.on("join", (data) => {
+        console.log("User: " + data.username + " joined " + data.room);
+        listUsers.push({
+            id: socket.id,
+            username: data.username,
+            room: data.room
+        });
+        socket.join(data.room);
+        let userroom = listUsers.filter(user => user.room === data.room)
+        io.to(data.room).emit("user_join", userroom);
+    });
+
+    socket.on("disconnecting", () => {
+        console.log("Left: " + socket.id);
+    });
+});
+
+
+server.listen(3000, () =>{
+    console.log(`App listening on: http://localhost:3000`);
+})
