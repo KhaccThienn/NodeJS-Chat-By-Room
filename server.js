dbf19254d435da87bf2c1d2407088bde2eb8db20
Lee Tien Sinh <muzankibut112@gmail.com>
2023-02-07 09:13:49 +0700

    Final Comment Code

--- server.js
+++ server.js
@@ -1,59 +1,59 @@
+// require những module cần thiết
 const express = require("express");
 const path = require("path");
 const app = express();
 
+// tạo server
 const server = require("http").createServer(app);
 
+// tạo io
 const io = require("socket.io")(server);
 
+// router đến file chat-app.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/chat-app.html'));
 });
 
+// khai báo mảng list user để lưu thông tin của tất cả user
 var listUsers = [];
 
-var rooms = [
-    {
-        id: "room-1",
-        name: "Mystical Minds"
-    },
-    {
-        id: "room-2",
-        name: "The Unusual Suspects"
-    },
-    {
-        id: "room-3",
-        name: "The Bizarre Bunch"
-    },
-    {
-        id: "room-4",
-        name: "The Odd Squad"
-    }
-];
-
-
+// mở kết nối với io.on
 io.on("connection", (socket) => {
+    // server lắng nghe sự kiện join
     socket.on("join", (data) => {
         console.log("User: " + data.username + " joined " + data.room);
+
+        // đẩy dữ liệu vào mảng list user
         listUsers.push({
             id: socket.id,
             username: data.username,
             room: data.room
         });
-
+        // tham gia vào phòng với đúng room-id
         socket.join(data.room);
+
+        // lấy ra danh sách user cùng thuộc 1 room
         let user_room = listUsers.filter(user => user.room === data.room);
 
+        // phát thông tin đến room theo đúng room-id, theo sự kiện user_join
         io.to(data.room).emit("user_join", user_room);
+
+        // phát message có người tham gia vào, theo sự kiện send
         io.to(data.room).emit("send", "<small class='text-danger'>System: " + data.username + " joined " + data.room + "</small>");
+        
+        // server lắng nghe sự kiện send-mess, sau đó lại phát ra message theo sự kiện send
         socket.on("send-mess", (mess) => {
             io.to(data.room).emit("send", `${data.username}: ${mess.content}`)
         });
+        // server lắng nghe sự kiện disconnect khi có người rời phòng chat
         socket.on("disconnecting", () => {
-            io.to(data.room).emit("send", "<small class='text-danger'>System: " +data.username + " left</small>"); 
+            // phát message có người thoát server
+            io.to(data.room).emit("send", "<small class='text-danger'>System: " +data.username + " left the room</small>"); 
         });
     });
 
+    // Đây là phần test =))))
+    // server lắng nghe sự kiện disconnect khi có người rời phòng chat
     socket.on("disconnecting", () => {
         console.log("Left: " + socket.id);
     });
